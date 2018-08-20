a530837df8775c0e19e67503aeeefade5cdb8274
alejandra-gonzalez <gonzalez.alejandra.013@gmail.com>
2018-08-20 13:21:57 -0700

    Add and Display Sample Data

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ The purpose of the Inventory App is to track the inventory of a retail store.
 Check lists are derived from the project rubrics created by Udacity.
 
 # Stage 1 Check List
-- [ ] A main activity that has methods to read data, a Contract Java class, and a DbHelper Java class
+- [x] A main activity that has methods to read data, a Contract Java class, and a DbHelper Java class
 
 - [x] A contract class that defines name of table and constants
 - [x] One inner class for each table in the contract class
@@ -13,8 +13,8 @@ Check lists are derived from the project rubrics created by Udacity.
 
 - [x] A subclass of SQLiteOpenHelper that overrides onCreate() and onUpgrade()
 
-- [ ] Single insert method that adds Product Name, Price, Quantity, Supplier Name, Supplier Phone Number
-- [ ] At least 2 different datatypes
+- [x] Single insert method that adds Product Name, Price, Quantity, Supplier Name, Supplier Phone Number
+- [x] At least 2 different datatypes
 
-- [ ] Single method that uses a Cursor from the database to perform a query on the table to retrieve at least one column of data
-- [ ] Said method should close the Cursor after reading from it
+- [x] Single method that uses a Cursor from the database to perform a query on the table to retrieve at least one column of data
+- [x] Said method should close the Cursor after reading from it
--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -1,13 +1,90 @@
 package com.example.android.inventoryapp;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.android.inventoryapp.data.ProductContract.ProductEntry;
+import com.example.android.inventoryapp.data.ProductDbHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProductDbHelper productDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        productDbHelper = new ProductDbHelper(this);
+
+        displayProductData();
+    }
+
+    /**
+     * Inserts a row of fake data.
+     */
+    public void insertProductData(View v){
+        SQLiteDatabase db = productDbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, "Krill Oil Capsules");
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, 12);
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, 25);
+        values.put(ProductEntry.COLUMN_SUPPLIER_NAME, "BioOptimal");
+        values.put(ProductEntry.COLUMN_SUPPLIER_PHONE_NUMBER, "(123) 456-7890");
+
+        long newRowId = db.insert(ProductEntry.TABLE_NAME, null, values);
+
+        if (newRowId != -1) {
+            displayProductData();
+        }
+    }
+
+    /**
+     * Displays all columns and rows in the database.
+     */
+    private void displayProductData(){
+        SQLiteDatabase db = productDbHelper.getReadableDatabase();
+
+        Cursor cursor = db.query(ProductEntry.TABLE_NAME, null, null,
+                null, null, null, null);
+
+        TextView displayView = findViewById(R.id.product_data);
+
+        String columnsHeader = ProductEntry.COLUMN_PRODUCT_NAME + " | " +
+                ProductEntry.COLUMN_PRODUCT_PRICE + " | " +
+                ProductEntry.COLUMN_PRODUCT_QUANTITY + " | " +
+                ProductEntry.COLUMN_SUPPLIER_NAME + " | " +
+                ProductEntry.COLUMN_SUPPLIER_PHONE_NUMBER + "\n";
+
+        if (!displayView.getText().toString().contains(columnsHeader)){
+            displayView.append(columnsHeader);
+        }
+
+        try {
+            int productNameColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_NAME);
+            int productPriceColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PRICE);
+            int productQuantityColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_QUANTITY);
+            int supplierNameColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_SUPPLIER_NAME);
+            int supplierNumberColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_SUPPLIER_PHONE_NUMBER);
+
+            while (cursor.moveToNext()) {
+                String currentProductName = cursor.getString(productNameColumnIndex);
+                int currentProductPrice = cursor.getInt(productPriceColumnIndex);
+                int currentProductQuantity = cursor.getInt(productQuantityColumnIndex);
+                String currentSupplierName = cursor.getString(supplierNameColumnIndex);
+                String currentSupplierNumber = cursor.getString(supplierNumberColumnIndex);
+
+                displayView.append(("\n" + currentProductName + " | " + currentProductPrice + " | " +
+                        currentProductQuantity + " | " + currentSupplierName + " | " + currentSupplierNumber));
+            }
+        } finally {
+            cursor.close();
+        }
     }
 }
